9f7f074dbdcf589d3d17de370afa0fb513a1448c
Emmely Lundberg <emmely.lundberg@noaignite.com>
2021-09-16 22:07:10 +0200

    animations project module

--- web/components/sections/ImageSection.js
+++ web/components/sections/ImageSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useInView } from "react-intersection-observer";
 import PropTypes from "prop-types";
 import imageUrlBuilder from "@sanity/image-url";
 import styles from "./ImageSection.module.css";
@@ -10,27 +11,33 @@ const builder = imageUrlBuilder(client);
 
 function ImageSection(props) {
   const { heading, label, text, image, cta } = props;
+  const [hovered, setHovered] = React.useState(false);
+  const toggleHover = () => setHovered(!hovered);
+
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+  });
 
   if (!image) {
     return null;
   }
 
   return (
-    <div className={styles.root}>
+    <div className={styles.root} ref={ref} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
       <figure className={styles.content}>
         <img
           src={builder.image(image).auto("format").width(2000).url()}
-          className={styles.image}
-          alt={heading}
+          className={`${styles.image} ${hovered ? styles.scaleImage : ""}`}
         />
 
-            <div className={styles.captionBox}>
-              <div className={styles.label}>{label}</div>
-              <h2 className={styles.title}>{heading}</h2>
-              <h4>Emmely</h4>
-              {text && <SimpleBlockContent blocks={text} />}
-              {cta && cta.route && <Cta {...cta} />}
-            </div>
+        <div className={styles.captionBox}>
+          <h2 className={`${styles.title} ${inView ? styles.fadeInText : ""}`}>{heading}</h2>
+
+          <span className={styles.text}>{text && <SimpleBlockContent blocks={text} />}</span>
+          {cta && cta.route && <Cta {...cta} />}
+          <div className={styles.label}>{label}</div>
+        </div>
       </figure>
     </div>
   );
